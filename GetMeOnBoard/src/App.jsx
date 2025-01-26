53c162084d65dc963909d713ff4a7a987a5d833f
tranmi60 <minhphuoc.tran@mail.utoronto.ca>
2025-01-26 02:13:39 -0500

    main now calls from app

--- GetMeOnBoard/src/App.jsx
+++ GetMeOnBoard/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
+import Quiz from './Quiz/Quiz';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Quiz />
+    </div>
   )
 }
 
